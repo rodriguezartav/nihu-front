597769d510ad46696f05bf55efaa5264d30f3442
Roberto <roberto@rodriguezartavia.com>
2018-03-24 07:05:01 -0600

    update ui

--- ui/apps/public/app.jsx
+++ ui/apps/public/app.jsx
@@ -9,8 +9,24 @@ class App extends React.Component {
 
 
   render(){
+      return <div>
+       <div className=" slds-m-top_medium slds-grid slds-grid_align-center slds-grid_vertical-align-center">
 
-    return <div>Nihu</div>
+        <div className="slds-col">
+
+          <img src="http://via.placeholder.com/600x450"/>
+
+        </div>
+
+
+
+    </div>
+
+
+    <div className="slds-text-align_center slds-m-top_small">
+      <a className="slds-text-title_caps" >Estamos en construcción</a>
+      </div>
+    </div>
   }
 }
 
